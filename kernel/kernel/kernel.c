136ec59432fb459400e3ad2183feade910c3fe7a
Alan <45009256+ejnAjaK3VgnnHBLk@users.noreply.github.com>
2019-11-29 21:26:01 -0700

    test

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #include <kernel/tty.h>
 
 void kernel_main(void) {
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
+	uint32_t bob = 322;
+	printf("Hello this is bob:%d", bob);
 }
