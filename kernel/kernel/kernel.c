929c07b1413eb8eb84d5c79973cf757e6b26d3d2
Alan <45009256+ejnAjaK3VgnnHBLk@users.noreply.github.com>
2019-11-29 21:54:46 -0700

    Update kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-#include <stdint.h>
 
 #include <kernel/tty.h>
 
 void kernel_main(void) {
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
-	uint32_t bob = 322;
-	printf("Hello this is bob:");
-	printf(bob);
 }
