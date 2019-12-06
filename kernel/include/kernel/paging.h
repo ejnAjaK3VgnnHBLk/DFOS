5cabaa3db68cf29b237ddbb4dfe4237016222eeb
Alan <45009256+ejnAjaK3VgnnHBLk@users.noreply.github.com>
2019-12-05 21:18:11 -0700

    Update paging.h

--- kernel/include/kernel/paging.h
+++ kernel/include/kernel/paging.h
@@ -1,6 +1,8 @@
 #ifndef _PAGING_KERN_H
 #define _PAGING_KERN_H
 
+#include <stdint.h>
+
 uint64_t page_dir_ptr_tab[4] __attribute__((aligned(0x20)));
 
 uint64_t page_dir[512] __attribute__((aligned(0x1000)));
