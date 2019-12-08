e0b03ebdd1b637618b56db04e2fe202aec37ab45
Alan <45009256+ejnAjaK3VgnnHBLk@users.noreply.github.com>
2019-12-07 20:15:06 -0700

    Update tty.c

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -5,12 +5,12 @@
 
 uint32 vga_index;
 static uint32 next_line_index = 1;
-uint8 g_fg = WHITE, g_bg = BLUE;
+uint8_t g_fg = WHITE, g_bg = BLUE;
 
 
-uint16 vga_entry(unsigned char ch, uint8 fg, uint8 bg) {
-    uint16 ax = 0;
-    uint8 ah = 0, al = 0;
+uint16_t vga_entry(unsigned char ch, uint8_t fg, uint8_t bg) {
+    uint16_t ax = 0;
+    uint8_t ah = 0, al = 0;
 
     ah = bg;
     ah <<= 4;
@@ -23,7 +23,7 @@ uint16 vga_entry(unsigned char ch, uint8 fg, uint8 bg) {
     return ax;
 };
 
-void clear_vga_buffer(uint16 **buffer, uint8 fg, uint8 bg) {
+void clear_vga_buffer(uint16_t **buffer, uint8_t fg, uint8_t bg) {
     for (uint32 i = 0; i < BUFSIZE; i++) {
         (*buffer)[i] = vga_entry(NULL, fg, bg);;
     }
@@ -32,8 +32,8 @@ void clear_vga_buffer(uint16 **buffer, uint8 fg, uint8 bg) {
 }
 
 // Start VGA buffer
-void init_vga(uint8 fg, uint8 bg) {
-    vga_buffer = (uint16*)VGA_ADDRESSES;
+void init_vga(uint8_t fg, uint8_t bg) {
+    vga_buffer = (uint16_t*)VGA_ADDRESSES;
     clear_vga_buffer(&vga_buffer, fg, bg);
     g_fg = fg;
     g_bg = bg;
